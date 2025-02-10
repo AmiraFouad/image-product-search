139f4dc522b63abf95970f1fba54f79ec9523751
Amira Fouad <79294411+AmiraFouad@users.noreply.github.com>
2025-02-10 13:51:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ import uuid
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+
+chromedriver_autoinstaller.install()
 
 chrome_options = Options()
 chrome_options.binary_location = "/usr/bin/google-chrome" 
