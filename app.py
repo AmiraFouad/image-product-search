0f92850b206b5d561611b151e172a504eb01b50f
Amira Fouad <79294411+AmiraFouad@users.noreply.github.com>
2025-02-10 14:10:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,12 +16,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 chrome_options = Options()
-chrome_options.binary_location = "/usr/bin/google-chrome" 
+chrome_options.binary_location = "/usr/bin/google-chrome"  
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-service = Service(ChromeDriverManager().install())
+# Use the correct ChromeDriver path inside Docker
+service = Service("/usr/local/bin/chromedriver")  
 driver = webdriver.Chrome(service=service, options=chrome_options)
 app = Flask(__name__)
 
