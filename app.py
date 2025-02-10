497ae2591f402af176a89ee5c4140b8c72141649
Amira Fouad <79294411+AmiraFouad@users.noreply.github.com>
2025-02-10 13:27:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 chrome_options = Options()
+chrome_options.binary_location = "/usr/bin/google-chrome" 
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
